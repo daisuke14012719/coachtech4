b72feec78945379c440086b6428ae64ad3475dc9
daisuke14012719 <79070611+daisuke14012719@users.noreply.github.com>
2021-04-13 13:51:24 +0900

    Add files via upload

---
+++ web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+
+
+Route::get('/building', [HelloController::class, 'index']);
+Route::get('/building/{room?}', [HelloController::class, 'end']);
